fe1fc01bbfe19cc75b190a3ce5b24d8534088c4c
victor <luo-kaisheng@foxmail.com>
2024-03-13 21:15:06 +0800

    <feature>: java web project demo add

---
+++ hygge-demo-java/web-project/src/main/java/fun/hygge/demo/java/webproject/WebProjectApplication.java
@@ -0,0 +1,11 @@
+package fun.hygge.demo.java.webproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebProjectApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WebProjectApplication.class, args);
+    }
+}
